922b44de9ae15755df685241be7064c86558cdf9
Mia van <82824582+MiavanRooijen@users.noreply.github.com>
2021-09-27 11:28:42 +0000

    class voor balletje gemaakt

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
- var x, y, v, z;
-  
+
+class Ball {
+  constructor(x, y, h, w, vx, vy) {
+    this.x = x
+    this.y = y
+    this.h = h
+    this.w = w
+    this.vx = vx
+    this.vy = vy
+  }
+
+  drawBall() {
+    ellipse(this.x, this.y, this.h, this.w);
+    this.x = this.x + this.vx
+    this.y = this.y + this.vy
+
+
+    if (this.x < 0 || this.x > 600) {
+      this.vx = this.vx * -1;
+    }
+
+    if (this.y < 0 || this.y > 400) {
+      this.vy = this.vy * -1;
+    }
+  }
+}
 
 function setup() {
-	createCanvas(600, 400);
+  createCanvas(600, 400)
 
-  x = 55;
-  y = 200;
-  v = 5;
-  z= 5;
-
+  ball1 = new Ball(0, 200, 10, 10, 5, 5)
 }
 
 function draw() {
-	background(225);
-  
-  ellipse(x,y,10,10);
-  x = x + v;
-  y = y + z;
+  background(225)
 
-  
-
-  if(x <= 0 || x > 600){
-    v = v * -1;
-  }
-
- if(y <= 0 || y > 400){
-    z = z * -1;
- }
-  
-  
+  ball1.drawBall()
 
 }
-	
